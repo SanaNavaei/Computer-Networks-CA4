805d0b1a1b47dc21e070c9e4a0e03fc7cba4832f
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2023-06-28 15:41:01 +0330

    add mechanisms

--- include/TCPconnection.hpp
+++ include/TCPconnection.hpp
@@ -2,13 +2,16 @@
 #define __TCPCONNECTION_HPP__
 
 #include <iostream>
+#include <vector>
 
 class TCPconnection 
 {
 private: 
+  std::vector<std::string> Mechanism = { "Slow Start", "Congestion Avoidence", "Fast Recovery", "Fast Retransmit"};
 
 public:
   TCPconnection();
+  std::vector<std::string> get_mechanisms();
   void run();
 }
 
