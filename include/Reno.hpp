9c1562861e588d7f4fa586c2fe8a0928dd229616
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2023-06-28 16:50:23 +0330

    add some attributes and methods

--- include/Reno.hpp
+++ include/Reno.hpp
@@ -12,9 +12,20 @@ class Reno
 {
 private: 
   std::vector<std::string> Mechanism;
+  std::string mechanism;
+  int cwnd;
+  int ssthresh;
+  int data_size;
+  int should_sent_again;
+  int not_ACKed_yet;
 
 public:
   Reno(Mechanism_/*attributes*/);
+  void change_mech(std::string newMech);
+  void Mult_cwnd(int num);
+  void decrement_size(int num);
+  void increment_cwnd(int num);
+  void SendData();
 }
 
 #endif
