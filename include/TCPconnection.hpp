f4cbc4660e0c25c955f2ad828b87fa7e50a95730
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2023-06-28 10:19:55 +0330

    Create TCPconnection.hpp
    add tcp connection class

---
+++ include/TCPconnection.hpp
@@ -0,0 +1,16 @@
+#ifndef __TCPCONNECTION_HPP__
+#define __TCPCONNECTION_HPP__
+
+#include <iostream>
+
+class TCPconnection 
+{
+private: 
+  int cwnd;
+  int ssthresh
+  int rtt;
+public:
+  TCPconnection();
+}
+
+#endif
