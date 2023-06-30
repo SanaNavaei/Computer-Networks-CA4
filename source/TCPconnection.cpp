2e7820183eac3b248f0fe23f57cba80ee929171c
SanaNavaei <sana.sarinavaei@gmail.com>
2023-06-30 14:20:48 +0330

    test reno algorithm

--- source/TCPconnection.cpp
+++ source/TCPconnection.cpp
@@ -1,14 +1,13 @@
 #include "../include/TCPconnection.hpp"
-#include "../include/Reno.hpp"
-#include "../include/NewReno.hpp"
-#include "../include/BBR.hpp"
 
 TCPconnection::TCPconnection();
 
 void TCPconnection::run()
 {
-    Reno reno_connnection(Mechanism/*attributes*/);
-    NewReno newReno_connection(Mechanism/*attributes*/);
-    BBR bbr_connection(Mechanism/*attributes*/);
+    Reno reno_connnection1(1, 1000);
+    reno_connnection1.run();
+    
+    //NewReno newReno_connection(Mechanism/*attributes*/);
+    //BBR bbr_connection(Mechanism/*attributes*/);
 }
 
