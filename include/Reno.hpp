5cca73bca6d65f62fdfd8dff5d1972990282b090
SanaNavaei <sana.sarinavaei@gmail.com>
2023-06-30 14:39:56 +0330

    Add OnPacketLoss func in header file
    Closes #3

--- include/Reno.hpp
+++ include/Reno.hpp
@@ -2,30 +2,40 @@
 #define __RENO_HPP__
 
 #include <iostream>
+#include <string>
+#include <vector>
 
 #define Slow_Start 0
-#define Congestion_Avoidence 1
+#define AIMD 1
 #define Fast_Recovery 2
-#define Fast_Retransmit 3
+
+#define DATA_SIZE 100
 
 class Reno
 {
 private: 
-  std::vector<std::string> Mechanism;
-  std::string mechanism;
+  std::vector<std::string> Mechanism = { "Slow Start", "AIMD", "Fast Recovery", "Fast Retransmit"};
+  int LastByteAcked;
+  int AckLostPacket;
+  int counter;
+  int mechanism;
   int cwnd;
   int ssthresh;
-  int data_size;
-  int should_sent_again;
-  int not_ACKed_yet;
+  int data_size = DATA_SIZE;
+  int rtt;
+  int timeout;
 
 public:
-  Reno(Mechanism_/*attributes*/);
+  Reno(int cwnd_, int ssthresh_);
+  void set_timeout();
   void change_mech(std::string newMech);
   void Mult_cwnd(int num);
+  void div_cwnd_by(int num);
   void decrement_size(int num);
   void increment_cwnd(int num);
   void SendData();
-}
+
+  int onPacketLoss();
+};
 
 #endif
