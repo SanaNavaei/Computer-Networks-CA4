705d2e9f639ee504ad85ebd6c88c26fb3ffd492d
SanaNavaei <sana.sarinavaei@gmail.com>
2023-06-30 23:29:23 +0330

    add functions in header file

--- include/BBR.hpp
+++ include/BBR.hpp
@@ -2,19 +2,45 @@
 #define __BBR_HPP__
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
 
-#define Slow_Start 0
-#define Congestion_Avoidence 1
-#define Fast_Recovery 2
-#define Fast_Retransmit 3
+#define Startup 0
+#define drain 1
+#define probeBW 2
+#define probeRTT 3
+
+#define DATA_SIZE 300000
 
 class BBR
 {
 private:
-  std::vector<std::string> Mechanism;
+  std::vector<std::string> Mechanism = { "Startup", "drain", "probeBW", "probeRTT"};
+  int mechanism;
+  int timeout;
+  int cwnd;
+  int LastByteAcked;
+  int counter = 0;
+  int rtt2 = 5;
+  int data_size = DATA_SIZE;
+  int acked[DATA_SIZE] = {0};
+  int rtt;
+  int drainage;
+  int probabilityCount;
+  int initialCwnd;
 
 public:
-  BBR(Mechanism_/*attributes*/);
-}
+  BBR(int cwnd_);
+  void change_mech(int newMech);
+  void Mult_cwnd(int num);
+  void SendData();
+  void onRTTUpdate();
+  void run();
+
+  int onPacketLoss();
+
+  bool lossProbability();
+};
 
 #endif
